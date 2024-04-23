6bd558f9fe1a9b57bd9d3cd6acc924bdb0879422
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2024-04-22 19:50:08 -0600

    feat: remove scripts, make bare repo

--- pull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Save directory of script
-scriptDir="$(dirname "$0")"
-
-# Copy all files from $HOME to this repo
-# shellcheck disable=SC2016
-find "$scriptDir" -type f -not -path "./.git/*" -not -path ./pull.sh -not -path ./push.sh \
-    -exec test -f "$HOME"/{} \; -exec sh -c 'cmp -s "$HOME/$1" "$1" || cp -u "$HOME/$1" "$1"' sh '{}' \;
